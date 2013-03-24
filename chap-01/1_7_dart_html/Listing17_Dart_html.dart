d2f15e72ef47d6cc50dbc57d6bf98ea0ffb754c8
Chris Buckett <chrisbuckett@gmail.com>
2013-03-24 10:44:07 +0000

    Button click handler changes and make dart.js local

--- chap-01/1_7_dart_html/Listing17_Dart_html.dart
+++ chap-01/1_7_dart_html/Listing17_Dart_html.dart
@@ -5,11 +5,13 @@ void main() {
   // or...
   // var button = new Element.tag("button");
   button.text = "Click me";
-  button.on.click.add((event) {
+  // button.on.click.add((event) { // on.click now deprecated
+  button.onClick.listen((event) { // now uses onClick.listen
     List buttonList = document.queryAll("button");
     window.alert("There is ${buttonList.length} button");
   });
 
-  document.body.nodes.add(button);
+  // document.body.nodes.add(button); // nodes is lower level
+  document.body.children.add(button); // recommend to use children
 
 }
