c76139d458d0e2f029ff68d13be50ff6b7182855
Chris Buckett <chrisbuckett@gmail.com>
2013-03-24 11:16:08 +0000

    Added dart2js output

--- chap-02/hello_world_app/HelloWorld.dart
+++
@@ -1,5 +0,0 @@
-/// This is the entry point function
-///  and outputs Hello World to the console
-main() {
-  print("Hello World");
-}
---
+++ chap-02/hello_world_dartdoc/HelloWorld.dart
@@ -0,0 +1,8 @@
+/** This is the entry point function
+ *  and outputs Hello World to the console
+ *  
+ *  Used for demonstrating DartDoc tool
+ */
+main() {
+  print("Hello World");
+}
