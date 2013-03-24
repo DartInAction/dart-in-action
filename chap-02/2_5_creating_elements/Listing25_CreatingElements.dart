04189431b5105250de6fa9bcf81bbe2cbc193392
Chris Buckett <chrisbuckett@gmail.com>
2013-03-24 11:12:14 +0000

    Fixed up html (already described in earlier examples)

--- chap-02/2_5_creating_elements/Listing25_CreatingElements.dart
+++ chap-02/2_5_creating_elements/Listing25_CreatingElements.dart
@@ -1,15 +1,16 @@
 import "dart:html";
 
 main() {
-  query("#status").innerHTML = "Hello World";
+  query("#status").innerHtml = "Hello World"; // innerHTML now innerHtml
+                                              // see code listing 2_4
 
   var button = new ButtonElement();
   button.text = "Click me";
-  button.on.click.add( (e) {
+  button.onClick.listen( (e) { // on.click.add now onClick.listen 
     var div = new Element.html("<div>I am a div element</div>");
-    document.body.elements.add(div);
+    document.body.children.add(div); // body.elements now body.children
   });
 
-  document.body.elements.add(button);
+  document.body.children.add(button);
 
 }
