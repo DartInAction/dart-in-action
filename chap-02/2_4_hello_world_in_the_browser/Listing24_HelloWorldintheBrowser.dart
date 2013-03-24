a58aff489c844b3b60451d2ec2921c854b77faf2
Chris Buckett <chrisbuckett@gmail.com>
2013-03-24 11:07:27 +0000

    Updated innerHTML to innerHtml

--- chap-02/2_4_hello_world_in_the_browser/Listing24_HelloWorldintheBrowser.dart
+++ chap-02/2_4_hello_world_in_the_browser/Listing24_HelloWorldintheBrowser.dart
@@ -2,5 +2,8 @@
 import 'dart:html';
 
 void main() {
-  query('#status').innerHTML = "Hello World";
+  query('#status').innerHtml = "Hello World"; // innerHTML now innerHtml
+  // The view has been taken that 2 char abbreviations are uppercase, but any
+  // longer should be camel case.  eg, window.indexedDB would be upper, 
+  // but HTML woud be Html
 }
